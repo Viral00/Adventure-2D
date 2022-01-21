5b16225029f51f9a0c522f815a6283e1e97ed57b
Viral_00 <viralghag@gmail.com>
2022-01-21 18:34:22 +0530

    2D Assets

---
+++ Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    public Animator animator;
+    bool jump;
+
+    private void Start()
+    {
+        jump = false;
+    }
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", speed);
+
+        if(jump = Input.GetButtonDown("Jump"))
+        {
+            animator.SetBool("Jump", jump); 
+        }
+        else
+        {
+            jump = false;
+        }
+    }
+}
